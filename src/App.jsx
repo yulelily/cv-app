8f16a2eb6a4860948ad99b334209da58005c833e
yulelily <124838371+yulelily@users.noreply.github.com>
2023-07-29 21:22:34 -0400

    Create general information card

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { v4 as uuid } from 'uuid'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Input({label, value}) {
+  const [text, setText] = useState(value);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <input type="text" placeholder={label} value={text} onChange={(e) => setText(e.target.value)}></input>
+  );
 }
 
-export default App
+function Card({children}) {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  );
+}
+
+function Education() {
+
+  const [items, setItems] = useState([]);
+
+  function handleAddBtn() {
+    const item = uuid();
+    setItems([...items, item]);
+  }
+
+  function handleRemoveBtn(e) {
+    const key = e.target.value;
+    setItems(items.filter(item => item !== key));
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>Education</h1>
+        <button onClick={handleAddBtn} >+</button>
+      </div>
+        <ul className="education-list" >
+          {items.map(item => <AddEducation key={item} value={item} />)}
+        </ul>
+    </div>
+  );
+
+  function AddEducation({value}) {
+    return (
+      <li>
+        <Card>
+          <Input label="School" value="School of Technology and Science" />
+          <Input label="Location" value="Somewhere, Somewhen" />
+        </Card>
+        <Card>
+          <Input label="Degree/GPA" value="Bachelor of Science in Computer Science; GPA: 4.00" />
+          <Input label="Date" value="Aug. 2021 - June. 2025" />
+        </Card>
+        <button value={value} onClick={handleRemoveBtn} >🗑</button>
+      </li>
+    );
+  }
+}
+
+export default function App() {
+  return (
+    <>
+      <Card>
+        <Input label="Name" value="Jane Doe" />
+        <label> Email: <Input label="Email" value="janedoe@gmail.com" /> </label>
+      </Card>
+      <Card>
+        <Input label="URL" value="https://www.janedoe.com" />
+        <label> Mobile: <Input label="TEL" value="+1-123-456-7890" /> </label>
+      </Card>
+      <Education />
+    </>
+  );
+}
